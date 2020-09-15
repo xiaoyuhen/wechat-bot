8b4b5d33039bc23a1c1825545fcb0c5c5cf8f778
xiaoyuhen <xiaoyuhen1994@gmail.com>
2020-09-15 19:14:04 +0800

    fix: listen

--- src/server.js
+++ src/server.js
@@ -1,11 +1,7 @@
 const createApp = require("./createApp");
 
-const port = process.env.PORT || 8000;
-
 const bootstrap = () => {
-  createApp().listen(port, () => {
-    console.info(`==> 🌎  Listening on port ${port}.`); // eslint-disable-line no-console
-  });
+  createApp();
 };
 
 bootstrap();
